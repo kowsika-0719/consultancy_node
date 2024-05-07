b93c85b23c220280953804c24ec58891162dd151
kowsika paramasivam <kowsip1903@gmail.com>
2024-05-07 20:22:02 +0530

    msg

---
+++ app.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+// const { connectedToMongoDB } = require("./configuration/dbConfig");
+const userRoutes = require("./src/routes/userRoute");
+const app = express();
+const port = process.env.port || 5000;
+
+// const path = require('path');
+
+// remove this(connectedToMongoDB) as we don't need to call this method from app.js
+// connectedToMongoDB().then(() => {
+//   app.listen(port, () => {
+//     console.log(`Server is running on http://localhost:${port}`);
+//   });
+// }) 
+
+app.use(bodyParser.json());
+app.use(cors());
+
+// app.use(express.static(path.join(__dirname, 'build')));
+
+app.use("/api/user",userRoutes);
+
+// app.get('/', (req, res) => {
+//   res.sendFile(path.json(__dirname, 'build', 'index.html'));
+// });
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
